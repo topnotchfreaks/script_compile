d8dc491f9e3846921e6b5358bd0d72cc0b99921e
Jackie <108690954+KDEFFALT@users.noreply.github.com>
2025-01-06 12:04:47 +0700

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -2,7 +2,6 @@
 
 WORK_DIR=$(pwd)
 KERNEL_DIR="msm-5.15"
-KERN_IMG=$WORK_DIR/out/android13-5.15/dist/Image
 
 cd $WORK_DIR
-LTO=thin BUILD_CONFIG=$KERNEL_DIR/build.config.gki.aarch64 build/build.sh
+LTO=thin BUILD_CONFIG=$KERNEL_DIR/build.config.msm.bengal build/build.sh
